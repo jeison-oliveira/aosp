25bb13d4e9dd887e19cec1c9860799b03449e764
joao <joaosilva2276@gmail.com>
2022-10-27 19:24:13 -0400

    Starting a new Project

---
+++ device/devtitans/curupira/AndroidProducts.mk
@@ -0,0 +1,7 @@
+# Adiciona o nome do produto no lunch
+COMMON_LUNCH_CHOICES := \
+    devtitans_curupira-eng
+
+# Arquivo de configuração principal do produto
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/devtitans_curupira.mk
