99f4d0968a021326b0ec92c777e85be233595045
joao <joaosilva2276@gmail.com>
2022-11-01 19:25:39 -0400

    changes in lab 5.5

--- device/devtitans/curupira/smartgas_lib/smartgas_lib.cpp
+++ device/devtitans/curupira/smartgas_lib/smartgas_lib.cpp
@@ -1,28 +1,22 @@
-#include "smartlamp_lib.h"
-
+#include "smartgas_lib.h"
 using namespace std;                                   // Permite usar string, ifstream diretamente ao invés de std::string
 using namespace android::base;                         // Permite usar GetBoolProperty ao invés de android::base::GetBoolProperty
-
-namespace devtitans::smartlamp {                       // Entra no pacote devtitans::smartlamp
-
-int Smartlamp::connect() {
-    char dirPath[] = "/sys/kernel/smartlamp";
+namespace devtitans::smartgas {                       // Entra no pacote devtitans::smartgas
+int Smartgas::connect() {
+    char dirPath[] = "/sys/kernel/smartgas";
     struct stat dirStat;
     if (stat(dirPath, &dirStat) == 0)
         if (S_ISDIR(dirStat.st_mode))
             return 1;                                  // Se o diretório existir, retorna 1
-
     // Diretório não existe, vamos verificar a propriedade
-    bool allowSimulated = GetBoolProperty("devtitans.smartlamp.allow_simulated", true);
+    bool allowSimulated = GetBoolProperty("devtitans.smartgas.allow_simulated", true);
     if (!allowSimulated)
         return 0;                                      // Dispositivo não encontrado
     else
         return 2;                                      // Usando valores simulados
 }
-
-int Smartlamp::readFileValue(string file) {
+int Smartgas::readFileValue(string file) {
     int connected = this->connect();
-
     if (connected == 2) {                               // Usando valores simulados
         if (file == "led")
             return this->simLedValue;
@@ -33,29 +27,19 @@ int Smartlamp::readFileValue(string file) {
             random_device dev;
             mt19937 rng(dev());
             uniform_int_distribution<mt19937::result_type> dist100(0,100);
-            return (int) dist100(rng);
-        }
-    }
-
+            return (int) dist100(rng); }}
     else if (connected == 1) {                          // Conectado. Vamos solicitar o valor ao dispositivo
         int value;
-        string filename = string("/sys/kernel/smartlamp/") + file;
+        string filename = string("/sys/kernel/smartgas/") + file;
         ifstream file(filename);                        // Abre o arquivo do módulo do kernel
-
         if (file.is_open()) {                           // Verifica se o arquivo foi aberto com sucesso
             file >> value;                              // Lê um inteiro do arquivo
             file.close();
-            return value;
-        }
-    }
-
+            return value; }}
     // Se chegou aqui, não foi possível conectar ou se comunicar com o dispositivo
-    return -1;
-}
-
-bool Smartlamp::writeFileValue(string file, int value) {
+    return -1;}
+bool Smartgas::writeFileValue(string file, int value) {
     int connected = this->connect();
-
     if (connected == 2) {                                // Usando valores simulados
         if (file == "led") {
             this->simLedValue = value;
@@ -64,42 +48,27 @@ bool Smartlamp::writeFileValue(string file, int value) {
         else if (file == "threshold") {
             this->simThresholdValue = value;
             return true;
-        }
-    }
-
+        }}
     else if (connected == 1) {                          // Conectado. Vamos solicitar o valor ao dispositivo
-        string filename = string("/sys/kernel/smartlamp/") + file;
+        string filename = string("/sys/kernel/smartgas/") + file;
         ofstream file(filename, ios::trunc);            // Abre o arquivo limpando o seu conteúdo
 
         if (file.is_open()) {                           // Verifica se o arquivo foi aberto com sucesso
             file << value;                              // Escreve o ledValue no arquivo
             file.close();
             return true;
-        }
-    }
-
+        }}
     // Se chegou aqui, não foi possível conectar ou se comunicar com o dispositivo
     return false;
 }
-
-int Smartlamp::getLed() {
-    return this->readFileValue("led");
-}
-
-bool Smartlamp::setLed(int ledValue) {
-    return this->writeFileValue("led", ledValue);
-}
-
-int Smartlamp::getLuminosity() {
-    return this->readFileValue("ldr");
-}
-
-int Smartlamp::getThreshold() {
-    return this->readFileValue("threshold");
-}
-
-bool Smartlamp::setThreshold(int thresholdValue) {
-    return this->writeFileValue("threshold", thresholdValue);
-}
-
+int Smartgas::getLed() {
+    return this->readFileValue("led");}
+bool Smartgas::setLed(int ledValue) {
+    return this->writeFileValue("led", ledValue);}
+int Smartgas::getLuminosity() {
+    return this->readFileValue("ldr");}
+int Smartgas::getThreshold() {
+    return this->readFileValue("threshold");}
+bool Smartgas::setThreshold(int thresholdValue) {
+    return this->writeFileValue("threshold", thresholdValue);}
 } // namespace
--- device/devtitans/curupira/smartgas_lib/smartgas_lib.h
+++ device/devtitans/curupira/smartgas_lib/smartgas_lib.h
@@ -1,20 +1,16 @@
 #pragma once                           // Inclui esse cabeçalho apenas uma vez
-
 #include <fstream>                     // Classe ifstream
 #include <android-base/properties.h>   // Função GetBoolProperty
 #include <sys/stat.h>                  // Função e struct stat
 #include <random>                      // Geração de números aleatórios (valores simulados)
-
 using namespace std;                   // Permite usar string diretamente ao invés de std::string
-
-namespace devtitans::smartlamp {       // Pacote Smartlamp
-
-class Smartlamp {
+namespace devtitans::smartgas {       // Pacote Smartgas
+class Smartgas {
     public:
         /**
-         * Verifica se o diretório /sys/kernel/smartlamp existe. Se existir
+         * Verifica se o diretório /sys/kernel/smartgas existe. Se existir
          * o dispositivo SmartLamp está conectado via USB. Caso contrário,
-         * verifica a propriedade devtitans.smartlamp.allow_simulated
+         * verifica a propriedade devtitans.smartgas.allow_simulated
          * para ver se valores simulados podem ser usados.
          *
          * Retorna:
@@ -23,30 +19,25 @@ class Smartlamp {
          *      2: simulado (disp. não encontrado, mas usando valores simulados)
          */
         int connect();
-
         /**
          * Acessa e seta a intensidade do led.
          *
          * Valor retornado/setado:
          *      0: led desligado
          *      1 a 99: led ligado com intensidade ledValue
-         *      100: led ligado com intensidade máxima
          */
         int getLed();
         bool setLed(int ledValue);
-
-
         /**
-         * Acessa o nível de luminosidade atual conforme reportado
-         * pelo sensor de luz -- LDR (light-dependent resistor).
+         * Acessa o nível gas
+         * pelo sensor de Gas-- MQ2.
          *
          * Retorna:
          *      0: completamente escuro
          *      1 a 99: nível de luminosidade
          *      100: completamente claro
          */
-        int getLuminosity();
-
+        int getGas();
         /**
          * Threshold - Limiar de luminosidade para ligar/desligar o led.
          * Ao executar esse método, o dispositivo SmartLamp entra no modo
@@ -64,18 +55,15 @@ class Smartlamp {
     private:
         /**
          * Métodos para ler e escrever valores nos arquivos "led",
-         * "ldr" ou "threshold" do diretório /sys/kernel/smartlamp.
+         * "gas" ou "threshold" do diretório /sys/kernel/smartgas.
          */
         int readFileValue(string file);
         bool writeFileValue(string file, int value);
-
         /**
          * Armazena valores simulados para o caso do dispositivo não estar
-         * conectado, mas a propriedade devtitans.smartlamp.allow_simulated
+         * conectado, mas a propriedade devtitans.smartgas.allow_simulated
          * for true.
          */
          int simLedValue = 88;
          int simThresholdValue = 42;
-};
-
-} // namespace
+};}
