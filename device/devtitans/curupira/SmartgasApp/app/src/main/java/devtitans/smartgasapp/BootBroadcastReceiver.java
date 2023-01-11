0f598e4d3d90b49c7543b24efb89ca6c298e7a61
Fabio Benigno <benigno.fabio@gmail.com>
2023-01-11 12:47:11 -0400

    Start service on boot
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

---
+++ device/devtitans/curupira/SmartgasApp/app/src/main/java/devtitans/smartgasapp/BootBroadcastReceiver.java
@@ -0,0 +1,12 @@
+package devtitans.smartgasapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        GasCheckScheduler.scheduleGasCheck(context);
+    }
+}
